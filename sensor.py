b43efe125c43a3fb495917a881307e0ff1a43fd3
saniho <nicolas.juignet@gmail.com>
2020-07-08 22:56:29 +0200

    first version

--- sensor.py
+++ sensor.py
@@ -41,28 +41,70 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 class myMeteo:
-    def __init__(self, token, insee):
+    def __init__(self, token, insee, _update_interval):
         self._token = token
         self._insee = insee
+        self._lastSynchro = None
+        self._update_interval = _update_interval
         pass
 
-    def __pluie(self, aCombienDeTemps):
-        pass
 
-    def _update(self):
-        # gestion d'un cache pour l'update...ou d'un timer pour eviter trop d'update ?
-        # gestion en locale des answer
+    def update(self):
         import json
+        import datetime
 
-        status_counts = defaultdict(int)
-        from urllib.request import urlopen
-        httpAnswer = urlopen(
-            'https://api.meteo-concept.com/api/forecast/nextHours?token=%s&insee=%s&hourly=true' % (self._token, self._insee))
-        self._forecast = json.loads(httpAnswer.read())['forecast']
+        courant = datetime.datetime.now()
+        #_LOGGER.warning("--------------")
+        #_LOGGER.warning("tente un update  ? ... %s" %(self._lastSynchro))
+        if ( self._lastSynchro == None ) or \
+            ( (self._lastSynchro + self._update_interval) < courant ):
+
+            #_LOGGER.warning("tente un update  ? ...")
+            #if ( self._lastSynchro != None):
+            #    _LOGGER.warning("self._lastSynchro + self._update_interval = %s" %(self._lastSynchro + self._update_interval) )
+            #    _LOGGER.warning("courant = %s" %(courant))
+
+            #_LOGGER.warning("update ...")
+            from urllib.request import urlopen
+            httpAnswer = urlopen(
+                'https://api.meteo-concept.com/api/forecast/nextHours?token=%s&insee=%s&hourly=true' % (self._token, self._insee))
+            self._forecast = json.loads(httpAnswer.read())['forecast']
+            self._lastSynchro = datetime.datetime.now()
+
+            #_LOGGER.warning("update fait, last synchro ... %s " %(self._lastSynchro))
         return self._forecast
 
+    def getPluieADelai(self, duree):
+        if duree == "1H":
+            return self._forecast[0]['rr10']
+        elif duree == "2H":
+            return self._forecast[1]['rr10']
+        elif duree == "3H":
+            return self._forecast[2]['rr10']
+        elif duree == "4H":
+            return self._forecast[3]['rr10']
+        elif duree == "5H":
+            return self._forecast[3]['rr10']
+        elif duree == "6H":
+            return self._forecast[3]['rr10']
+        elif duree == "7H":
+            return self._forecast[3]['rr10']
 
-
+    def getProbaPluieDelai(self, duree):
+        if duree == "1H":
+            return self._forecast[0]['probarain']
+        elif duree == "2H":
+            return self._forecast[1]['probarain']
+        elif duree == "3H":
+            return self._forecast[2]['probarain']
+        elif duree == "4H":
+            return self._forecast[3]['probarain']
+        elif duree == "5H":
+            return self._forecast[3]['probarain']
+        elif duree == "6H":
+            return self._forecast[3]['probarain']
+        elif duree == "7H":
+            return self._forecast[3]['probarain']
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the platform."""
@@ -80,19 +122,24 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         _LOGGER.exception("Could not run my First Extension")
         return False
 
-    mMeteo = myMeteo( token, insee)
-    add_entities([cumulPluieA1h(session, name, update_interval, mMeteo)], True)
-    add_entities([probabilitePluieA1h(session, name, update_interval, mMeteo)], True)
+    mMeteo = myMeteo( token, insee, update_interval)
+    mMeteo.update()
+    for i in [ "1H", "2H", "3H", "4H", "5H", "6H", "7H"]:
+        add_entities([cumulPluieAh(session, name, update_interval, mMeteo, insee, i)], True)
+        add_entities([probabilitePluieAh(session, name, update_interval, mMeteo, insee, i)], True)
     # on va gerer  un element par heure ... maintenant
 
-class cumulPluieA1h(Entity):
+class cumulPluieAh(Entity):
     """cumulPluieA1h."""
 
-    def __init__(self, session, name, interval, myMeteo):
+    def __init__(self, session, name, interval, myMeteo, insee, delai):
         """Initialize the sensor."""
         self._session = session
         self._name = name
         self._myMeteo = myMeteo
+        self._insee = insee
+        self._delai = delai
+        self._interval = interval
         self._attributes = None
         self._state = None
         self.update = Throttle(interval)(self._update)
@@ -100,7 +147,7 @@ class cumulPluieA1h(Entity):
     @property
     def name(self):
         """Return the name of the sensor."""
-        return "myMeteo.cumulPluieEstime.1h"
+        return "myMeteo.%s.cumulPluieEstime.%s" %(self._insee, self._delai)
 
     @property
     def state(self):
@@ -114,9 +161,9 @@ class cumulPluieA1h(Entity):
 
     def _update(self):
         """Update device state."""
-        forecast = self._myMeteo._update()
+        self._myMeteo.update()
         status_counts = defaultdict(int)
-        status_counts[0] = forecast[0]['rr10']
+        status_counts[0] = self._myMeteo.getPluieADelai( self._delai )
 
         self._attributes = {ATTR_ATTRIBUTION: ""}
         self._attributes.update(status_counts)
@@ -132,14 +179,16 @@ class cumulPluieA1h(Entity):
         """Icon to use in the frontend."""
         return ICON
 
-class probabilitePluieA1h(Entity):
+class probabilitePluieAh(Entity):
     """probabilitePluieA1h."""
 
-    def __init__(self, session, name, interval, myMeteo):
+    def __init__(self, session, name, interval, myMeteo, insee, delai):
         """Initialize the sensor."""
         self._session = session
         self._name = name
         self._myMeteo = myMeteo
+        self._insee = insee
+        self._delai = delai
         self._attributes = None
         self._state = None
         self.update = Throttle(interval)(self._update)
@@ -147,7 +196,7 @@ class probabilitePluieA1h(Entity):
     @property
     def name(self):
         """Return the name of the sensor."""
-        return "myMeteo.probabilitePluieEstime.1h"
+        return "myMeteo.%s.probabilitePluieEstime.%s" %(self._insee, self._delai)
 
     @property
     def state(self):
@@ -162,9 +211,9 @@ class probabilitePluieA1h(Entity):
     def _update(self):
         """Update device state."""
 
-        forecast = self._myMeteo._update()
+        self._myMeteo.update()
         status_counts = defaultdict(int)
-        status_counts[0] = forecast[0]['probarain']
+        status_counts[0] = self._myMeteo.getProbaPluieDelai(self._delai)
 
         self._attributes = {ATTR_ATTRIBUTION: ""}
         self._attributes.update(status_counts)
